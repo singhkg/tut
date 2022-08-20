4ed0e23d7a23479391e1cd58c8397317199547e9
gaurav <kgsingh81@gmail.com>
2022-08-20 09:37:32 +0530

    module2-solution

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,14 +1,39 @@
 (function () {
 'use strict';
-var x1 = function () {
-  // do something, PLEASE!
-  return "Blah!";
+angular.module('ShopList', [])
+.controller('ShopListController', ShopListController)
+.controller('ShopListShowController', ShopListShowController)
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('BuyService',BuyService);
+
+ToBuyController.$inject=['BuyService'];
+ShopListShowController.$inject=['BuyService'];
+
+function ShopListShowController(BuyService){
+  var showlist = this;
+  showlist.itemstobuy=BuyService.getitem();
+}
+
+function ToBuyController(BuyService){
+  var service = this;
+  var itemtoadd = this;
+  itemtoadd.additem = function () {
+    BuyService.additem();
+  };
+
+function BuyService(){
+  var itemstobuy = [];
+  service.additem = function () {
+  itemstobuy.push({'Rice', 10});
+  itemstobuy.push({'Dal', 10});
+  itemstobuy.push({'roti', 10});
+  itemstobuy.push({'namkeen', 10});
+  itemstobuy.push({'cookie', 10});
 };
-
-angular.module('FirstAngular', [])
-
-.controller('firstController', function ($scope){
-$scope.name="gaurav";
-
-});
-})();
+service.getitem = function () {
+  return itemstobuy;
+};
+}
+$scope.itemstobuy=itemstobuy;
+});();
